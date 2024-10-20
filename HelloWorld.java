2cb482da08054df2d19b50e12d2786755d194ac2
Abdul Hameed <sd.hameed79@gmail.com>
2024-10-20 21:15:57 +0530

    First Java program added

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// write a simple java program to print Hello World program
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
